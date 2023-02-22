79bf46d6c4a86b4b811bbf7f7764314c54c9b5b5
bdavis83 <bdavis83@gmail.com>
2023-02-22 14:12:12 -0500

    wrote return statement for HandleSubmit function in CreatePostForm component

---
+++ social-feed-project/src/CreatePostForm/CreatePostForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './CreatePostForm.css';
+
+const CreatePostForm = (props) => {
+    
+    const [name, setName] = useState (''); //setName is function that is called to update the name variable
+    const [post, setPost] = useState ('')
+
+    function handleSubmit (event){
+        event.preventDefault(); // stops entire page from refreshing adter submitting
+        let newEntry ={
+            name: name,
+            post: post
+        };
+        console.log(newEntry)
+        props.CreatePostFormProperty(newEntry)
+    }
+    
+    return ( 
+        <form onSubmit={handleSubmit} className='form-grid'>
+            <div className='form-group'>
+                <label>Name</label>
+                <input type='text' className='form-control' value={name} onChange={(event)=>setName(event.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Post</label>
+                <inpout type='message' className='form-control' value={post} onChange={(event)=>setPost(event.target.value)}/>
+            </div>
+         </form>
+     );
+}
+ 
+export default CreatePostForm;
